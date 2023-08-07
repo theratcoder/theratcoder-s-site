c8d3f027d9c82955030ceffa3753e6e8e8dfccb1
theratcoder <50275635+theratcoder@users.noreply.github.com>
2023-08-07 16:51:50 +0000

    Initial prep for transition to Replit from Heroku
    Removed code that will now be broken and have added many Replit-specific files (automatic)

--- index.js
+++ index.js
@@ -2,26 +2,20 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const fetch = require('node-fetch');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 const app = express();
-const { Pool } = require('pg');
 const { strict } = require('assert');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+
 const aac = require('./aac');
 
 const users = [
-	"Geoff",
-	"Tim"
+  "Geoff",
+  "Tim"
 ];
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
-	extended: false
+  extended: false
 }));
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -30,195 +24,185 @@ app.set('view engine', 'ejs')
 app.use('/aac', aac);
 
 app.get('/', (req, res) => {
-	res.render('pages/index');
+  res.render('pages/index');
 });
 
 app.get('/search', async (req, res) => {
-	var GET = req.query;
+  var GET = req.query;
 
-	if (GET.terms == undefined) {
-		res.render("pages/search-no-res");
-	}
+  if (GET.terms == undefined) {
+    res.render("pages/search-no-res");
+  }
 
-	var words = GET.terms.split(' ');
-	var results = [];
+  var words = GET.terms.split(' ');
+  var results = [];
 
-	var pagesFolder = await fs.promises.readdir("views/pages");
-	var sims = await fs.promises.readdir("views/pages/sims");
-	var games = await fs.promises.readdir("views/pages/games");
+  var pagesFolder = await fs.promises.readdir("views/pages");
+  var sims = await fs.promises.readdir("views/pages/sims");
+  var games = await fs.promises.readdir("views/pages/games");
 
-	var pages = [];
-	var titles = [];
+  var pages = [];
+  var titles = [];
 
-	for (ppage of pagesFolder) {
-		if (ppage.substr(ppage.length - 4, ppage.length - 1) == ".ejs")
-			pages.push(ppage);
-	}
+  for (ppage of pagesFolder) {
+    if (ppage.substr(ppage.length - 4, ppage.length - 1) == ".ejs")
+      pages.push(ppage);
+  }
 
-	for (var i = 0; i < pages.length; i++)
-		pages[i] = "/" + pages[i];
-	for (var i = 0; i < sims.length; i++)
-		sims[i] = "/sims/" + sims[i];
-	for (var i = 0; i < games.length; i++)
-		games[i] = "/games/" + games[i];
-	
-	pages = pages.concat(sims, games);
+  for (var i = 0; i < pages.length; i++)
+    pages[i] = "/" + pages[i];
+  for (var i = 0; i < sims.length; i++)
+    sims[i] = "/sims/" + sims[i];
+  for (var i = 0; i < games.length; i++)
+    games[i] = "/games/" + games[i];
 
-	for (page of pages) {
-		var text = fs.readFileSync(`views/pages${page}`, "utf-8");
-		titles.push(text.split('<title>')[1].split('</title>')[0]);
-	}
+  pages = pages.concat(sims, games);
 
-	for (var i = 0; i < pages.length; i++)
-		pages[i] = pages[i] != "/index.ejs" ? pages[i].substr(0, pages[i].length - 4) : "/";
+  for (page of pages) {
+    var text = fs.readFileSync(`views/pages${page}`, "utf-8");
+    titles.push(text.split('<title>')[1].split('</title>')[0]);
+  }
 
-	for (word of words) {
-		for (var i = 0; i < pages.length; i++) {
-			if (pages[i].toLowerCase().includes(word.toLowerCase()) ||
-				titles[i].toLowerCase().includes(word.toLowerCase()))
-			{
-				results.push({
-					title: titles[i],
-					link: pages[i]
-				});
+  for (var i = 0; i < pages.length; i++)
+    pages[i] = pages[i] != "/index.ejs" ? pages[i].substr(0, pages[i].length - 4) : "/";
 
-				pages.splice(i, 1);
-				titles.splice(i, 1);
+  for (word of words) {
+    for (var i = 0; i < pages.length; i++) {
+      if (pages[i].toLowerCase().includes(word.toLowerCase()) ||
+        titles[i].toLowerCase().includes(word.toLowerCase())) {
+        results.push({
+          title: titles[i],
+          link: pages[i]
+        });
 
-				i--;
-			}
-		}
-		/*
-		for (user of users) {
-			if (user.toLowerCase().includes(word.toLowerCase()))
-				results.push(user);
-		}
-		*/
-	}
-	
-	if (results.length == 0) results.push("We didn't find anything on our site that matched your search.");
-	
-	var results = {
-		'search': GET.terms,
-		'results': results
-	};
-	res.render('pages/search', results);
+        pages.splice(i, 1);
+        titles.splice(i, 1);
+
+        i--;
+      }
+    }
+    /*
+    for (user of users) {
+      if (user.toLowerCase().includes(word.toLowerCase()))
+        results.push(user);
+    }
+    */
+  }
+
+  if (results.length == 0) results.push("We didn't find anything on our site that matched your search.");
+
+  var results = {
+    'search': GET.terms,
+    'results': results
+  };
+  res.render('pages/search', results);
 });
 
 app.get('/helpful-licence', (req, res) => {
-	res.render('pages/helpful-licence');
+  res.render('pages/helpful-licence');
 });
 
 app.get('/private-policy-helpful', (req, res) => {
-	res.render('pages/private-policy-helpful');
+  res.render('pages/private-policy-helpful');
 });
 
 app.get('/support', (req, res) => {
-	res.render('pages/support');
+  res.render('pages/support');
 });
 app.post('/support', async (req, res) => {
-	var name = req.body.name;
-	var message = req.body.message;
-	var email = req.body.email;
-	
-	try {
-	   const client = await pool.connect();
-	   var result = await client.query('INSERT INTO support_requests VALUES($1, $2, $3)', [name, message, email]);
-	   
-	   res.send("Thank you, we are working on resolving the issue. Click <a href=\"/\">here</a> to return to the homepage.");
-	   client.release();
-	} catch (err) {
-	   console.error(err);
-	   res.send("Error " + err);
-	}
-	
+  var name = req.body.name;
+  var message = req.body.message;
+  var email = req.body.email;
+
+  // TODO: update this to work correctly on replit
 });
 
 app.get('/helpful-functions', (req, res) => {
-	res.render('pages/helpful-functions');
+  res.render('pages/helpful-functions');
 });
 
 app.get('/helpful', (req, res) => {
-	res.render('pages/helpful-home');
+  res.render('pages/helpful-home');
 });
 
 app.get('/sims', async (req, res) => {
-	var sims = [];
-	var files = await fs.promises.readdir("views/pages/sims");
+  var sims = [];
+  var files = await fs.promises.readdir("views/pages/sims");
 
-	for (var i = 0; i < files.length; i++) {
-		var text;
+  for (var i = 0; i < files.length; i++) {
+    var text;
 
-		try {
-			text = fs.readFileSync(`views/pages/sims/${files[i]}`, "utf-8");
-		}
-		catch (err) {
-			res.send(`Uh oh, something went wrong.\nError: ${err}`);
-			return;
-		}
+    try {
+      text = fs.readFileSync(`views/pages/sims/${files[i]}`, "utf-8");
+    }
+    catch (err) {
+      res.send(`Uh oh, something went wrong.\nError: ${err}`);
+      return;
+    }
 
-		var title = text.split('<title>')[1].split('</title>')[0];
-		
-		sims.push({
-			title: title,
-			link: "/sims/" + files[i].substr(0, files[i].length - 4)
-		});
-	}
+    var title = text.split('<title>')[1].split('</title>')[0];
 
-	sims = {"sims": sims};
-	res.render('pages/sims', sims);
+    sims.push({
+      title: title,
+      link: "/sims/" + files[i].substr(0, files[i].length - 4)
+    });
+  }
+
+  sims = { "sims": sims };
+  res.render('pages/sims', sims);
 });
 
 app.get('/sims/:sim_name', (req, res) => {
-	var simName = req.params.sim_name;
-	if (fs.existsSync(`views/pages/sims/${simName}.ejs`))
-		res.render(`pages/sims/${simName}`);
-	else
-		res.send("Sorry, the page you requested does not exist.");
+  var simName = req.params.sim_name;
+  if (fs.existsSync(`views/pages/sims/${simName}.ejs`))
+    res.render(`pages/sims/${simName}`);
+  else
+    res.send("Sorry, the page you requested does not exist.");
 });
 
 app.get('/games', async (req, res) => {
-	var games = [];
-	var files = await fs.promises.readdir("views/pages/games");
+  var games = [];
+  var files = await fs.promises.readdir("views/pages/games");
 
-	for (var i = 0; i < files.length; i++) {
-		var text;
+  for (var i = 0; i < files.length; i++) {
+    var text;
 
-		try {
-			text = fs.readFileSync(`views/pages/games/${files[i]}`, "utf-8");
-		}
-		catch (err) {
-			res.send(`Uh oh, something went wrong.\nError: ${err}`);
-			return;
-		}
+    try {
+      text = fs.readFileSync(`views/pages/games/${files[i]}`, "utf-8");
+    }
+    catch (err) {
+      res.send(`Uh oh, something went wrong.\nError: ${err}`);
+      return;
+    }
 
-		var title = text.split('<title>')[1].split('</title>')[0];
-		
-		games.push({
-			title: title,
-			link: "/games/" + files[i].substr(0, files[i].length - 4)
-		});
-	}
+    var title = text.split('<title>')[1].split('</title>')[0];
 
-	games = {"games": games};
-	res.render('pages/games', games);
+    games.push({
+      title: title,
+      link: "/games/" + files[i].substr(0, files[i].length - 4)
+    });
+  }
+
+  games = { "games": games };
+  res.render('pages/games', games);
 });
 
 app.get('/games/:game_name', (req, res) => {
-	var gameName = req.params.game_name;
-	if (fs.existsSync(`views/pages/games/${gameName}.ejs`)) {
-		var text = fs.readFileSync(`views/pages/games/${gameName}.ejs`, "utf-8");
-		var title = text.split('<title>')[1].split('</title>')[0];
+  var gameName = req.params.game_name;
+  if (fs.existsSync(`views/pages/games/${gameName}.ejs`)) {
+    var text = fs.readFileSync(`views/pages/games/${gameName}.ejs`, "utf-8");
+    var title = text.split('<title>')[1].split('</title>')[0];
 
-		var game = {"game": {
-				title: title,
-				file: gameName + ".ejs"
-			}
-		};
-		res.render("pages/game-player", game);
-	}
-	else
-		res.send("Sorry, the page you requested does not exist.");
+    var game = {
+      "game": {
+        title: title,
+        file: gameName + ".ejs"
+      }
+    };
+    res.render("pages/game-player", game);
+  }
+  else
+    res.send("Sorry, the page you requested does not exist.");
 });
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
